1f34470ef827a3ee242df9418216a4d3d7806cf4
ivan Ivinlol <lolivinlol@gmail.com>
2026-03-08 22:32:30 +0000

    nada, puede ser que los estilos aplicados a la clase no estén aplicando?

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -22,22 +22,24 @@ function Calendar({
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex flex-col gap-2 justify-center text-center pt-1 relative items-center mb-4",
+        caption: "flex flex-col gap-1.5 justify-center text-center pt-1 relative items-center mb-2",
         caption_label: "text-sm font-medium",
-        nav: "space-x-4 flex items-center",
+        nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
         nav_button_previous: "",
         nav_button_next: "",
-        table: "w-full border-collapse",
+        table: "w-full border-collapse space-y-1",
         head_row: "",
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] text-center",
-        row: "",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-range-start)]:rounded-l-md focus-within:relative focus-within:z-20",
-        day: "h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-md transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+        row: "w-full mt-2",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-range-start)]:rounded-l-md [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        day: cn(
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-md transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+        ),
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
@@ -47,7 +49,7 @@ function Calendar({
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground rounded-none",
+          "aria-selected:bg-accent aria-selected:text-accent-foreground !rounded-none",
         day_hidden: "invisible",
         ...classNames,
       }}
